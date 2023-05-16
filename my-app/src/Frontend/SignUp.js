acd3b7aef365ad1c573cdbee9be1d0d6029b62dc
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-16 15:45:55 +0530

    Added input Tags and buttons

---
+++ my-app/src/Frontend/SignUp.js
@@ -0,0 +1,26 @@
+export function Signup1()
+{
+    return(
+        <div>
+            <div className="loginBox" style={{marginTop:"12%"}}>
+    <div>
+    <p style={{textAlign:"center",fontSize:"35px",marginBottom:"35px"}}>Login</p>
+     
+     <input placeholder={"User Name"} style={{padding:"6px 6px",margin:"4px"}}></input>
+     <input placeholder={"Password"} style={{padding:"6px 6px",margin:"4px"}}></input>
+     
+     <button style={{
+     padding: "5px 5px",
+     fontSize: "16px",
+     backgroundColor: "#4A4A4B",
+     color: "white",
+     borderRadius:"3px",
+     marginTop: "10px",
+   }}>Register</button>
+   
+    </div>
+   
+</div>
+        </div>
+    )
+}
