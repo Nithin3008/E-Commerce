79c1953ea49dff8d127e33f67587b41c0e29142a
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-16 15:45:41 +0530

    Added signup Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import {Routes,Route} from "react-router-dom"
 import { Home1 } from "./Frontend/Home"
 import {Login1} from "./Frontend/Login"
+import { Signup1 } from "./Frontend/SignUp";
 function App() {
   return (
     <div className="App">
-      <Login1></Login1>
+      {/* <Login1></Login1> */}
       <Routes>
         <Route path="Home1" element={<Home1></Home1>}></Route>
         <Route path="/Login1" element={<Login1 />}></Route>
+        <Route path="/Signup1" element={<Signup1 />}></Route>
       </Routes>
 
     </div>
