c56321a418225c750a23adf5ae53ce2d2dd04d2c
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-16 14:50:25 +0530

    Added home and Login routes

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import {Routes,Route} from "react-router-dom"
+import { Home1 } from "./Frontend/Home"
+import {Login1} from "./Frontend/Login"
+function App() {
+  return (
+    <div className="App">
+      <Home1></Home1>
+      <Routes>
+        <Route path="Home1" element={<Home1></Home1>}></Route>
+        <Route path="/Login1" element={<Login1 />}></Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
