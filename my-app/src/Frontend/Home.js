7e01f2751b1f622c7122b760cdca5787b54906a0
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-16 10:00:45 +0530

    made new changes

---
+++ my-app/src/Frontend/Home.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+import pic from "./images/mainPage/truck.jpg";
+import pic1 from "./images/mainPage/car.jpg";
+import pic2 from "./images/mainPage/suv.jpg";
+import pic3 from "./images/mainPage/ev.jpg";
+import pic4 from "./images/recent/ferrari.jpg";
+import pic5 from "./images/recent/dodge.jpg";
+
+export function Home1() {
+  return (
+   <div>
+    
+   </div> 
+    
+  )
+}
